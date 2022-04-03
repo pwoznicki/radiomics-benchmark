eb42c4f9fe8e57f5063b279782bf96b6c1f17eb8
Piotr Woźnicki <piotrekwoznicki@gmail.com>
2022-04-03 22:29:53 +0200

    add inferrer for worc task

--- worc/training.py
+++ worc/training.py
@@ -1,40 +1,39 @@
 import pandas as pd
 from classrad.data.dataset import FeatureDataset
 from classrad.models.classifier import MLClassifier
-from classrad.training.trainer import Trainer
+from classrad.preprocessing.preprocessor import Preprocessor
+from classrad.training.trainer import Inferrer, Trainer
+from classrad.utils import io
 
 import config
 
-dataset_name = config.DATASETS[0]
-feature_df = pd.read_csv(
-    config.WORC_DIR / "results" / dataset_name / "features.csv"
-)
-features = [
-    col
-    for col in feature_df.columns
-    if col.startswith(("original", "wavelet", "log-sigma"))
-]
-feature_dataset = FeatureDataset(
-    dataframe=feature_df,
-    features=features,
-    target="label",
-    ID_colname="subject_id",
-    task_name=f"{dataset_name}",
-    meta_columns=["subject_id", "label"],
-)
-split_path = config.RESULT_DIR / dataset_name / "splits.json"
-feature_dataset.full_split(split_path)
-feature_dataset.load_splits_from_json(split_path)
-model = MLClassifier.from_sklearn(name="Random Forest")
-model.set_optimizer("optuna", n_trials=5)
 
-trainer = Trainer(
-    dataset=feature_dataset,
-    models=[model],
-    result_dir=config.RESULT_DIR,
-    feature_selection="anova",
-    num_features=10,
-    experiment_name="exp1",
-)
+def main():
+    task_name = config.DATASETS[0]
+    result_dir = config.RESULT_DIR / task_name
+    feature_df = pd.read_csv(result_dir / "features.csv")
+    feature_dataset = FeatureDataset(
+        dataframe=feature_df,
+        target="label",
+        ID_colname="subject_id",
+    )
+    splits_path = result_dir / "splits.json"
+    feature_dataset.full_split(splits_path)
+    feature_dataset.load_splits_from_json(splits_path)
+    models = MLClassifier.initialize_default_sklearn_models()
+    trainer = Trainer(
+        dataset=feature_dataset,
+        models=models,
+        result_dir=result_dir,
+        experiment_name=task_name,
+    )
+    trainer.set_optimizer("optuna", n_trials=15)
+    # trainer.run(auto_preprocess=True)
 
-trainer.run()
+    best_params = io.load_json(result_dir / "best_params.json")
+    inferrer = Inferrer(params=best_params, result_dir=result_dir)
+    inferrer.fit_eval(feature_dataset.data)
+
+
+if __name__ == "__main__":
+    main()
